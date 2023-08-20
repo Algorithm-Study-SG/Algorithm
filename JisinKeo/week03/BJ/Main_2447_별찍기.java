c6aed68d3f36f55fc7640c205109962eca787caf
JisinKeo <tntjd9514@naver.com>
2023-08-20 19:22:35 +0900

    Solved: 3주차 2447 별찍기

--- JisinKeo/week03/BJ/Main_2447_별찍기.java
+++ JisinKeo/week03/BJ/Main_2447_별찍기.java
@@ -1,16 +1,60 @@
 package JisinKeo.week03.BJ;
 
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
+/**
+ * 메모리 : 25952 KB
+ * 시간 : 336 ms
+ */
 public class Main_2447_별찍기 {
 
-    static int n;
+    static int N;
 
-    public static void main(String[] args) throws IOException{
+    static char[][] result;
+
+    public static void calculate(int n, int i, int j){
+
+        if (n < 3){ // n == 1과 같다
+            result[i][j] = '*';
+            return;
+        }
+        calculate(n / 3, i, j);
+        calculate(n / 3, i, j + n / 3);
+        calculate(n / 3, i, j + 2 * n / 3);
+        calculate(n / 3, i + n / 3, j);
+        // calculate(n / 3, i + n / 3, j + n / 3); 가운데는 빈 곳이므로 이것을 빼야 한다
+        calculate(n / 3, i + n / 3, j + 2 * n / 3);
+        calculate(n / 3, i + 2 * n / 3, j);
+        calculate(n / 3, i + 2 * n / 3, j + n / 3);
+        calculate(n / 3, i + 2 * n / 3, j + 2 * n / 3);
+    }
+
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+
+        result = new char[N][N];
+
+        for(int i = 0; i < N; i++){
+            Arrays.fill(result[i], ' ');
+        }
+
+        calculate(N, 0, 0);
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                bw.write(result[i][j]);
+            }
+            bw.write("\n");
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
 }

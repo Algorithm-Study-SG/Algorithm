5286ba25d8d98633c91a79a329cd6adaff4a5ab7
snowman2810 <140784660+snowman2810@users.noreply.github.com>
2023-10-20 22:07:17 +0900

    Delete snowman2810/week10/SQL/solution_133027_주문량이_많은_아이스크림들_조회하기.sql

--- snowman2810/week10/SQL/solution_133027_주문량이_많은_아이스크림들_조회하기.sql
+++
@@ -1,8 +0,0 @@
-select f.flavor
-from first_half f 
-join (
-    select sum(total_order) total_order, flavor 
-    from july 
-    group by flavor
-) j on j.flavor=f.flavor
-order by f.total_order+j.total_order desc limit 3;
